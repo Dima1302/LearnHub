a4442359fe10e40302bc02fa7e03e0e1f5f1c2c4
Dmitriy <Dimitriy303@yandex.ru>
2023-06-30 23:14:04 +0900

    in progress

--- LearnHub/src/main/java/com/example/LearnHub/controllers/UserController.java
+++ LearnHub/src/main/java/com/example/LearnHub/controllers/UserController.java
@@ -4,71 +4,62 @@ import com.example.LearnHub.dto.RecommendationDTO;
 import com.example.LearnHub.models.Achievement;
 import com.example.LearnHub.models.Badge;
 import com.example.LearnHub.models.User;
-import com.example.LearnHub.services.AchievementService;
-import com.example.LearnHub.services.BadgeService;
 import com.example.LearnHub.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/user")
+@Controller
 public class UserController {
-private final UserService userService;
-    private final BadgeService badgeService;
-    private final AchievementService achievementService;
 
-@Autowired
-    public UserController(UserService userService, BadgeService badgeService, AchievementService achievementService) {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
         this.userService = userService;
-    this.badgeService = badgeService;
-    this.achievementService = achievementService;
-}
-
-    @GetMapping("/{userId}/progress")
-    public ResponseEntity<List<RecommendationDTO>> getUserProgress(@PathVariable Integer userId) {
-        List<RecommendationDTO>userProgress = userService.getUserProgress(userId);
-        return ResponseEntity.ok(userProgress);// Реализация получения списка выполненных рекомендаций пользователя
     }
 
-    @PostMapping("/{userId}/recommendations/{recommendationId}/complete")
-    public ResponseEntity<String> completeRecommendation(@PathVariable int userId, @PathVariable int recommendationId) {
-        // Логика завершения рекомендации пользователем
-       userService.completeRecommendation(userId,recommendationId);  // Пример: userService.completeRecommendation(userId, recommendationId);
+    @GetMapping("/user/{userId}/progress")
+    public ResponseEntity<List<RecommendationDTO>> getUserProgress(@PathVariable Integer userId) {
+        List<RecommendationDTO> userProgress = userService.getUserProgress(userId);
+        return ResponseEntity.ok(userProgress);
+    }
 
+    @PostMapping("/user/{userId}/recommendations/{recommendationId}/complete")
+    public ResponseEntity<String> completeRecommendation(@PathVariable int userId, @PathVariable int recommendationId) {
+        userService.completeRecommendation(userId, recommendationId);
         return ResponseEntity.ok("Recommendation completed successfully");
     }
 
-
-
-    @GetMapping("/{userId}/badges")
+    @GetMapping("/user/{userId}/badges")
     public ResponseEntity<List<Badge>> getUserBadges(@PathVariable int userId) {
         User user = userService.getUserById(userId);
         List<Badge> badges = user.getBadges();
         return ResponseEntity.ok(badges);
     }
 
-    @GetMapping("/{userId}/achievements")
+    @GetMapping("/user/{userId}/achievements")
     public ResponseEntity<List<Achievement>> getUserAchievements(@PathVariable int userId) {
         User user = userService.getUserById(userId);
         List<Achievement> achievements = user.getAchievements();
         return ResponseEntity.ok(achievements);
     }
 
-    @PostMapping("/{userId}/badges/assign")
+    @PostMapping("/user/{userId}/badges/assign")
     public ResponseEntity<String> assignBadgeToUser(@PathVariable int userId) {
         userService.assignBadgeToUser(userId);
         return ResponseEntity.ok("Badge assigned successfully");
     }
 
-    @PostMapping("/{userId}/achievements/complete")
+    @PostMapping("/user/{userId}/achievements/complete")
     public ResponseEntity<String> completeAchievement(@PathVariable int userId) {
         userService.completeAchievement(userId);
         return ResponseEntity.ok("Achievement completed successfully");
     }
-
-
-
 }
+
+
+
--- LearnHub/src/main/java/com/example/LearnHub/dto/UserDTO.java
+++ LearnHub/src/main/java/com/example/LearnHub/dto/UserDTO.java
@@ -6,7 +6,7 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 public class UserDTO {
-    private int id;
+
     @NotEmpty(message = "Name should not be empty")
     @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String username;
@@ -14,9 +14,19 @@ public class UserDTO {
     @NotEmpty(message = "Email should not be empty")
     private String email;
 
-
     @NotBlank(message = "Password cannot be empty")
     private String password;
+
+    private int id;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getUsername() {
         return username;
@@ -42,12 +52,5 @@ public class UserDTO {
         this.password = password;
     }
 
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
+    // Getters and setters
 }
