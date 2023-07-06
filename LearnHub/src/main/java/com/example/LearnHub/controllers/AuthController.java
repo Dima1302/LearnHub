73ad6a90a92ae08cd2789f2f40dd3fec8b65a10f
Dmitriy <Dimitriy303@yandex.ru>
2023-07-07 00:33:09 +0900

    in progress

--- LearnHub/src/main/java/com/example/LearnHub/controllers/AuthController.java
+++ LearnHub/src/main/java/com/example/LearnHub/controllers/AuthController.java
@@ -2,7 +2,7 @@ package com.example.LearnHub.controllers;
 
 
 import com.example.LearnHub.dto.UserDTO;
-import com.example.LearnHub.models.User;
+
 import com.example.LearnHub.services.UserService;
 import com.example.LearnHub.util.UserValidator;
 
@@ -14,13 +14,33 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
 
     private final UserValidator personValidator;
     private final UserService userService;
+    private final Logger logger = LoggerFactory.getLogger(AuthController.class);
+
+    // остальной код контроллера
+
+    @PostMapping("/registration")
+    public String performRegistration(@ModelAttribute("user") @RequestBody @Valid UserDTO userDTO, BindingResult bindingResult) {
+        logger.debug("Performing user registration...");
+
+        personValidator.validate(userDTO, bindingResult);
+        if (bindingResult.hasErrors()) {
+            logger.debug("Validation errors detected: {}", bindingResult.getAllErrors());
+            return "auth/registration";
+        }
+
+        userService.createUser(userDTO);
+        logger.debug("User registration successful. Redirecting to /auth/login");
+        return "redirect:/auth/login";
+    }
+
 
     public AuthController(UserValidator personValidator, UserService userService) {
         this.personValidator = personValidator;
@@ -35,30 +55,24 @@ public class AuthController {
 
     // Остальные методы контроллера без изменений
 
-    @PostMapping("/registration")
-    public String performRegistration(@ModelAttribute("user") @RequestBody @Valid UserDTO userDTO, BindingResult bindingResult) {
-        personValidator.validate(userDTO, bindingResult);
-        if (bindingResult.hasErrors()) {
-            return "auth/registration";
-        }
 
-        userService.createUser(userDTO); // Используем метод createUser из UserService, передавая userDTO
 
-        return "redirect:/auth/login";
-    }
+
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody UserDTO userDTO) {
+    public String login() {
         // остальной код метода...
-        return ResponseEntity.ok().build();
+        return "redirect:/home"; // замените "home" на вашу целевую страницу
     }
 
 
-    private User convertUserDtoToUser(UserDTO userDTO) {
-        // Преобразование UserDTO в User
-        User user = new User();
-        user.setUsername(userDTO.getUsername());
-        user.setPassword(userDTO.getPassword());
-        user.setSportLevel(userDTO.getSportLevel());
-        return user;
+
+    @GetMapping("/login")
+    public String showLoginForm(Model model) {
+        model.addAttribute("user", new UserDTO());
+        return "auth/login";
     }
+
+
+
+
 }
